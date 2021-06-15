f715493e1719833f16faf16efbee83dadb76e217
jiho <hanjiho@kakao.com>
2021-06-15 23:43:28 +0900

    2021. 06. 15

--- src/main/java/com/ddori/book/springboot/web/HelloController.java
+++ src/main/java/com/ddori/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.ddori.book.springboot.web;
 
+import com.ddori.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
 }
